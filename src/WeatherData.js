144b5e67b7a1d87515915ebf3ad8045913c85636
SowmyaPadamati19 <padamatisowmya7@gmail.com>
2024-06-16 16:58:36 +0530

    Update WeatherData.js

--- src/WeatherData.js
+++ src/WeatherData.js
@@ -1,13 +1,13 @@
 // src/data/weatherData.js
 const weatherData = {
     'Mumbai': {
-        name: 'New York',
+        name: 'Mumbai',
         weather: [{ description: 'clear sky' }],
         main: { temp: 36, humidity: 65 },
         wind: { speed: 5 }
     },
-    'Karnataka': {
-        name: 'London',
+    'Banglore': {
+        name: 'Banglore',
         weather: [{ description: 'mist' }],
         main: { temp: 40, humidity: 80 },
         wind: { speed: 3 }
